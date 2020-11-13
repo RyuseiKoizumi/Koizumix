9bfafcda3b7715ae10cd41fa09dea1fa205833ad
RyuseiKoizumi <alh.rkoizumi@gmail.com>
2020-11-13 10:29:45 +0900

    change icon and try ignoring

--- src/App.js
+++ src/App.js
@@ -11,12 +11,12 @@ function App() {
       <Nav />
       <Banner />
       <Row
-        title="NETFLIX ORIGINALS"
+        title="NETFLIXオリジナル作品"
         fetchUrl={requests.fetchNetflixOriginals}
         isLargeRow
       />
-      <Row title="Trending Now" fetchUrl={requests.fetchTrending} />
-      <Row title="Top Rated" fetchUrl={requests.fetchTrending} />
+      <Row title="人気急上昇の作品" fetchUrl={requests.fetchTrending} />
+      <Row title="今日の総合TOP10" fetchUrl={requests.fetchTrending} />
       <Row title="Action Movies" fetchUrl={requests.fetchActionMovies} />
       <Row title="Comedy Movies" fetchUrl={requests.fetchComedyMovies} />
       <Row title="Horror Movies" fetchUrl={requests.fetchHorrorMovies} />
